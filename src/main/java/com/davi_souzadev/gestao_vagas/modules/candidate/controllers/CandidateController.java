f425ad7e64b0a7d1698b625211ddce71abfaa316
Davi Souza <dnsbr7@gmail.com>
2024-07-05 18:50:48 -0300

    update CandidateController

--- src/main/java/com/davi_souzadev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/davi_souzadev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,11 +1,17 @@
 package com.davi_souzadev.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.davi_souzadev.gestao_vagas.modules.candidate.CandidateEntity;
+import com.davi_souzadev.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 
 import jakarta.validation.Valid;
 
@@ -13,10 +19,28 @@ import jakarta.validation.Valid;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
+    // @GetMapping("/")
+    // public ResponseEntity<List<CandidateEntity>> getAll() {
+    //     var candidates = candidateRepository.findAll();
+    //     return ResponseEntity.status(HttpStatus.OK).body(candidates);
+    // }
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
+
+    // @DeleteMapping("/{id}")
+    // public ResponseEntity<String> delete(@PathVariable UUID id) {
+    //     candidateRepository.deleteById(id);
+    //     return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
+    // }
 }
